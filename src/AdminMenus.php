7eff8e4fd8ff2ed3d87535a680250e8a0ac08d20
Puleeno Nguyen <puleeno@gmail.com>
2019-08-17 11:31:36 +0700

    Remove admin menu

--- src/AdminMenus.php
+++ src/AdminMenus.php
@@ -14,6 +14,6 @@ class AdminMenus
         $main_menu_title = apply_filters('jankx_menu_title', __('Jankx Settings', 'jankx'));
         $main_menu_label = apply_filters('jankx_menu_label', __('Jankx', 'jankx'));
 
-        add_menu_page($main_menu_title, $main_menu_label, 'manage_options', 'jankx', null, null, 65);
+        // add_menu_page($main_menu_title, $main_menu_label, 'manage_options', 'jankx', null, null, 65);
     }
 }
